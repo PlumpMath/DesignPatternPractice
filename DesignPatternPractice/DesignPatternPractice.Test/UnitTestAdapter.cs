59459c60be88f46b3c082f01cc52256a527073c5
Thkouob Chen <thkouob@gmail.com>
2016-06-22 01:09:47 +0800

    add adapter model simple example

---
+++ DesignPatternPractice/DesignPatternPractice.Test/UnitTestAdapter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesignPatternPractice.Test
+{
+    [TestClass]
+    public class UnitTestAdapter
+    {
+        [TestMethod]
+        public void Test_Adapter()
+        {
+            ITarget adapter = new MyAdapter();
+            adapter.Request();
+        }
+    }
+}
---
+++ DesignPatternPractice/DesignPatternPractice/Adapater/Adaptee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternPractice
+{
+    public class Adaptee
+    {
+        public void SpecificRequest()
+        {
+            Console.WriteLine("Hello adaptee!!");
+        }
+    }
+}
